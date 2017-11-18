c918cf864fcc68cdadb8c045389a050a0bd6298a
Vennittor <ventor_prime@hotmail.com>
2017-11-18 09:08:22 -0800

    Imported Testing Area for Weapons

---
+++ Assets/Scripts/Inventory/Testing/TestInteraction.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInteraction : MonoBehaviour
+{
+	enum State {IDLE, USINGABILITY};
+
+	[SerializeField] private State state;
+
+	public Animator myAnimator;
+	public Weapon weaponOne;
+	public Weapon weaponTwo;
+	private IUsableItem iUseOne;
+	private IUsableItem iUseTwo;
+
+
+
+	void Start () 
+	{
+		myAnimator = GetComponent<Animator> ();
+
+		iUseOne = weaponOne.GetComponent<IUsableItem> ();
+		iUseTwo = weaponTwo.GetComponent<IUsableItem> ();
+
+		EquipWeapon (weaponOne);
+		EquipWeapon (weaponTwo);
+
+	}
+	
+	void Update ()
+	{
+		if (state == State.IDLE) 
+		{
+			
+			if (Input.GetMouseButtonDown (0)) 
+			{
+				iUseOne.StartUsingItem ();
+				state = State.USINGABILITY;
+			}
+
+			if (Input.GetMouseButtonDown (1))
+			{
+				iUseTwo.StartUsingItem ();
+				state = State.USINGABILITY;
+			}
+
+		}
+
+		if (state == State.USINGABILITY)
+		{
+			if (Input.GetMouseButtonDown (0)) 
+			{
+				iUseOne.StartUsingItem ();
+			} 
+			else if (Input.GetMouseButton (0)) 
+			{
+				iUseOne.UsingItem ();
+			} 
+			else if (Input.GetMouseButtonUp (0)) 
+			{
+				iUseOne.StopUsingItem ();
+			}
+
+
+
+			if (Input.GetMouseButtonDown (1))
+			{
+				iUseTwo.StartUsingItem ();
+			}
+			else if (Input.GetMouseButton(1))
+			{
+				iUseTwo.UsingItem ();
+			}
+			else if (Input.GetMouseButtonUp (1)) 
+			{
+				iUseTwo.StopUsingItem ();
+			}
+
+		}
+
+	}
+
+	void EquipWeapon(Weapon weapon)
+	{
+		weapon.Equip(myAnimator, gameObject.transform);
+	}
+}
