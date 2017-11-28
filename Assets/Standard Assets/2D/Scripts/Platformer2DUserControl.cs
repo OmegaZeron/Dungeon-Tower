aa10657829c2eb1b9f6ddda1089cbcc7835140d8
Unknown <omegazeron@yahoo.com>
2017-11-27 19:02:09 -0800

    Adding interactible check to player, adding states
    Will move states to game manager at a later date

--- Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -8,6 +8,7 @@ namespace UnityStandardAssets._2D
     public class Platformer2DUserControl : MonoBehaviour
     {
         private enum ControllerState { playerControl, menuControl, noControl };
+        ControllerState controlState;
         private PlatformerCharacter2D m_Character;
         private bool m_Jump;
         private bool m_JumpHeld;
@@ -16,44 +17,51 @@ namespace UnityStandardAssets._2D
         private void Awake()
         {
             m_Character = GetComponent<PlatformerCharacter2D>();
+            controlState = ControllerState.playerControl;
         }
 
 
         private void Update()
         {
-            if (!m_Jump)
+            if (controlState == ControllerState.playerControl)
             {
-                // Read the jump input in Update so button presses aren't missed.
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-            }
+                if (!m_Jump)
+                {
+                    // Read the jump input in Update so button presses aren't missed.
+                    m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+                }
 
-            if (!m_JumpHeld)
-            {
-                m_JumpHeld = CrossPlatformInputManager.GetButton("Jump");
+                if (!m_JumpHeld)
+                {
+                    m_JumpHeld = CrossPlatformInputManager.GetButton("Jump");
+                }
             }
         }
 
 
         private void FixedUpdate()
         {
-            // Read the inputs.
-            bool crouch = Input.GetKey(KeyCode.LeftControl);
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-			float v = CrossPlatformInputManager.GetAxis ("Vertical");
-            //if (!m_Jump)
-            //{
-            //    // Read the jump input in Update so button presses aren't missed.
-            //    m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-            //}
+            if (controlState == ControllerState.playerControl)
+            {
+                // Read the inputs.
+                bool crouch = Input.GetKey(KeyCode.LeftControl);
+                float h = CrossPlatformInputManager.GetAxis("Horizontal");
+                float v = CrossPlatformInputManager.GetAxis("Vertical");
+                //if (!m_Jump)
+                //{
+                //    // Read the jump input in Update so button presses aren't missed.
+                //    m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+                //}
 
-            //if (!m_JumpHeld)
-            //{
-            //    m_JumpHeld = CrossPlatformInputManager.GetButton("Jump");
-            //}
-            // Pass all parameters to the character control script.
-            m_Character.Move(h, v, crouch, m_Jump, m_JumpHeld);
-            m_Jump = false;
-            m_JumpHeld = false;
+                //if (!m_JumpHeld)
+                //{
+                //    m_JumpHeld = CrossPlatformInputManager.GetButton("Jump");
+                //}
+                // Pass all parameters to the character control script.
+                m_Character.Move(h, v, crouch, m_Jump, m_JumpHeld);
+                m_Jump = false;
+                m_JumpHeld = false;
+            }
         }
     }
 }
