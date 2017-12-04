1526e99d5f8cf632ca582dc9ec1309691cbc9700
Unknown <omegazeron@yahoo.com>
2017-12-04 14:26:14 -0800

    Setting up Character base class

---
+++ Assets/Scripts/CharacterController/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour {
+
+    protected float m_MaxSpeed;
+    protected float m_JumpForce;
+    protected int health;
+    protected bool m_FacingRight;
+    protected List<Collider2D> playerColliders;
+    protected Animator m_Anim;
+
+    //public void Move(float move, float verticalAxis, bool crouch, bool jump, bool jumpHeld)
+    //{
+
+    //}
+
+    protected void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
