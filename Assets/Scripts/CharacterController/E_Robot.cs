21dadddefe2bbfb5ff75e42258f4ff08508bdf0d
Tandy Dang <aznnoodle101@hotmail.com>
2017-12-11 06:33:02 -0800

    Stable Enough changes to Robot
    *Patrol() works back and forth
    *MoveTo() works when aggro'd to Player, stays on Player when in vision with OverlapBox check
    *WallCheck disabled, maybe obsolete now

--- Assets/Scripts/CharacterController/E_Robot.cs
+++ Assets/Scripts/CharacterController/E_Robot.cs
@@ -9,8 +9,10 @@ public class E_Robot : CombatCharacter {
     private float runSpeed = 1;
     [SerializeField] private uint damageDealt;
     [SerializeField] private float knockback;
-    [SerializeField] private CircleCollider2D playerCheck;
+    [SerializeField] private BoxCollider2D playerCheck;
     private Collider2D[] check;
+
+    [SerializeField] private GameObject player;
 
     // Use this for initialization
     void Start () {
@@ -25,7 +27,7 @@ public class E_Robot : CombatCharacter {
         {
             if (collider.name.Equals("PlayerCheck"))
             {
-                playerCheck = (CircleCollider2D)collider;
+                playerCheck = (BoxCollider2D)collider;
             }
         }
 	}
@@ -35,7 +37,7 @@ public class E_Robot : CombatCharacter {
         Patrol();
 	}
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision) // Damage Player
     {
         if (collision.gameObject == GameManager.instance.Player.gameObject)
         {
@@ -43,33 +45,69 @@ public class E_Robot : CombatCharacter {
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision) // Detect Player
     {
         if (collision.gameObject == GameManager.instance.Player.gameObject)
         {
+            player = GameManager.instance.Player.gameObject;
             StartCoroutine("AttackPlayer");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject == GameManager.instance.Player.gameObject) {
+            player = null;
+            m_Rigidbody2D.velocity = m_Rigidbody2D.velocity.normalized;
         }
     }
 
     IEnumerator AttackPlayer()
     {
-        yield return null;
+        while(player != null) 
+        {
+            MoveTo(player.transform.position, 20);
+            yield return null;
+        }
+    }
+
+    private void MoveTo(Vector3 position, float speed)
+    {
+        Debug.Log("Robot MoveTo" + player.name);
+        Vector3 target = new Vector3(position.x, position.y, 0); // Tandy: this corrects position
+        Vector3 offset = new Vector3(position.x - transform.position.x, 0, 0);
+        if(offset.x > 0 && !m_FacingRight || offset.x < 0 && m_FacingRight)
+        {
+            Flip();
+        }
+        //m_Rigidbody2D.MovePosition((target * 0.1f) + target * speed * Time.deltaTime);
+        m_Rigidbody2D.velocity = offset.normalized * speed;
+
+        Collider2D c;
+        if (m_FacingRight) {
+            c = Physics2D.OverlapBox(transform.position + new Vector3(.75f, 0, 0), new Vector2(.75f, 2), 0f, LM);
+        }
+        else {
+            c = Physics2D.OverlapBox(transform.position + new Vector3(-.75f, 0, 0), new Vector2(.75f, 2), 0f, LM);
+        }
+        if (c != null) {
+            TakeKnockback(knockback / 2);
+        }
     }
 
     private void Patrol() 
     {
-        Debug.Log("m_FacingRight: " + m_FacingRight);
+        //Debug.Log("m_FacingRight: " + m_FacingRight);
         if (m_FacingRight)
         {
             m_Rigidbody2D.MovePosition(transform.position + new Vector3(.1f, 0, 0));
-            Collider2D c = Physics2D.OverlapBox(transform.position + new Vector3(.7f, 0, 0), new Vector2(.5f, 2), 0f, LM);
+            Collider2D c = Physics2D.OverlapBox(transform.position + new Vector3(.75f, 0, 0), new Vector2(.75f, 2), 0f, LM);
             if(c != null) {
                 Flip();
             }
         }
         else {
             m_Rigidbody2D.MovePosition(transform.position + new Vector3(-.1f, 0, 0));
-            Collider2D c = Physics2D.OverlapBox(transform.position + new Vector3(-.7f, 0, 0), new Vector2(.5f, 2), 0f, LM);
+            Collider2D c = Physics2D.OverlapBox(transform.position + new Vector3(-.75f, 0, 0), new Vector2(.75f, 2), 0f, LM);
             if (c != null) {
                 Flip();
             }
@@ -78,10 +116,10 @@ public class E_Robot : CombatCharacter {
 
     void OnDrawGizmos() {
         if (m_FacingRight) {
-            Gizmos.DrawWireCube(transform.position + new Vector3(.7f, 0, 0), new Vector3(.5f, 2, 0));
+            Gizmos.DrawWireCube(transform.position + new Vector3(.75f, 0, 0), new Vector3(.75f, 2, 0));
         }
         else {
-            Gizmos.DrawWireCube(transform.position + new Vector3(-.7f, 0, 0), new Vector3(.5f, 2, 0));
+            Gizmos.DrawWireCube(transform.position + new Vector3(-.75f, 0, 0), new Vector3(.75f, 2, 0));
         }
     }
 }
